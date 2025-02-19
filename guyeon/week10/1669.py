d87bb7a4b286af7fc76f86d20583eecef314327c
Gouyeon Chung <gouyeonch@naver.com>
2025-02-20 00:31:30 +0900

    14225

--- guyeon/week10/1669.py
+++ guyeon/week10/1669.py
@@ -13,14 +13,9 @@ cnt = 1
 now = 1
 cumul = 1
 while cumul < dest:
-    # print(f"cumul : {cumul}")
-    # print(f"now : {now}")
-    # print()
-
     for i in (1,0,-1):
         tmp = now + i
         e = ((tmp+1)*(tmp))//2
-        # print(i)
 
         if e+cumul < dest:
             now +=i
@@ -30,8 +25,6 @@ while cumul < dest:
                 cnt += tmp
                 print(cnt)
                 exit()
-
-
     cnt += 1
 
 print(cnt)
---
+++ guyeon/week11/14225.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+S = list(map(int, input().split()))
+
+arr = [False] * (2000009)
+
+def dfs(depth,idx,ss):
+    if depth == N:
+        return
+    
+    ss+=S[idx]
+    arr[ss]=True
+    dfs(depth+1,idx+1,ss)
+    dfs(depth+1,idx+1,ss-S[idx])
+
+dfs(0,0,0)
+
+for i in range(1,2000009):
+    if not arr[i]:
+        print(i)
+        exit()
